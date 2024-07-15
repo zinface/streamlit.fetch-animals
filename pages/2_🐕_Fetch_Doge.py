206742fa0900375adfbb337ad52d3c4e600ef5b3
zinface <zinface@163.com>
2024-07-15 11:10:04 +0800

    repo: 调整页面统一操作函数

--- pages/2_🐕_Fetch_Doge.py
+++ pages/2_🐕_Fetch_Doge.py
@@ -16,7 +16,7 @@ def getImageLink():
 def getImage(url):
     return requests.get(url).content
 
-def getDogeBytesIO():
+def getImageBytesIO():
     container = io.BytesIO()
     container.write(getImage(getImageLink()))
     return container
@@ -24,7 +24,7 @@ def getDogeBytesIO():
 loading_bar = st.progress(0.0, '加载中')
 time.sleep(0.1)
 loading_bar.progress(0.1, '加载中')
-image = getDogeBytesIO()
+image = getImageBytesIO()
 loading_bar.progress(1.0, '加载完成')
 time.sleep(0.1)
 loading_bar.image(image)
